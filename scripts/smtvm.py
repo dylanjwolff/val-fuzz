c0f9293fc63f5c65a35a1f1d5634143444cb281a
wolffdy <wolffdy0@gmail.com>
2020-04-30 03:27:22 +0200

    using actual README instructions for z3

--- scripts/smtvm.py
+++ scripts/smtvm.py
@@ -14,9 +14,9 @@ def in_bin(commit):
 
 def z3(commit):
     def build(commit):
-        os.system("cd .solvers/z3; rm -rf .solvers/z3/build")
         if os.system("cd .solvers/z3; git checkout " + commit) != 0: raise "not a commit"
-        os.system("cd .solvers/z3; ./configure -d; cd build; make -j 4")
+        os.system("cd .solvers/z3; rm -rf .solvers/z3/build")
+        os.system("cd .solvers/z3; python scripts/mk_make.py; ./configure -d; cd build; make -j 4")
         os.system("mv .solvers/z3/build/z3 "+"bin/z3-"+commit+";chmod +x" +" bin/z3-"+commit)
     if (not in_bin(commit)):
         build(commit)
@@ -25,8 +25,8 @@ def z3(commit):
 
 def cvc4(commit):
     def build(commit):
-        os.system("cd .solvers/cvc4; rm -rf .solvers/cvc4/build")
         if os.system("cd .solvers/cvc4; git checkout " + commit) != 0: raise "not a commit"
+        os.system("cd .solvers/cvc4; rm -rf .solvers/cvc4/build")
         os.system("cd .solvers/cvc4; ./contrib/get-antlr-3.4; ./configure.sh --assertions; cd build; make -j 4")
         os.system("mv .solvers/cvc4/build/bin/cvc4"+" bin/cvc4-"+commit+ ";chmod +x" +" bin/cvc4-"+commit)
     if (not in_bin(commit)):
