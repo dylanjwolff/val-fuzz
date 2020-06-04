1cee02aedabd8d1490fc4bf02e8e1ae5e5c71017
Dylan Wolff <wolffd@amdsuplus1.inf.ethz.ch>
2020-06-04 14:28:58 +0200

    tweaking sample sampling

--- scripts/grab_samps.sh
+++ scripts/grab_samps.sh
@@ -1,1 +1,1 @@
-find . -maxdepth 1 -type d \( ! -name . \) -exec zsh -c 'cd {} && ls **/*.smt2 | shuf | head -2 ' \;
+find . -maxdepth 1 -type d \( ! -name . \) -exec zsh -c 'cd {} && ls **/*.smt2 | shuf | head -250 | xargs -n 1 mv -t ../../samples' \;
