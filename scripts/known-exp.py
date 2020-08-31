2a7222d3bf2c69c037f5e6d587e099e51a8f6f13
wolffdy <wolffdy0@gmail.com>
2020-09-01 01:49:42 +0200

    updating script a bit

--- scripts/known-exp.py
+++ scripts/known-exp.py
@@ -7,98 +7,117 @@ import json
 from pandas.api.types import CategoricalDtype
 from tqdm import tqdm
 
-config_tag = "BASE"
-repetitions = 30
 
-base_cvc4 = "0675545"
-base_z3 = "a35d00e"
+configs = {
+        "ALL1": 1,
+        "ALL5": 5,
+        "ALL10": 10,
+        "ALL15": 15,
+        "ALL20": 20,
+        "ALL25": 25,
+        "ALL30": 30,
+        "ALL40": 40,
+        "ALL55": 55,
+        "ALL70": 70,
+        "ALL90": 90,
+        "ALL130": 130,
+        "ALL160": 160,
+        "ALL200": 200,
+}
 
-o = sp.getoutput("ls ~/known/repro").split("\n")
-solv_hashes = [l.split("-") for l in o]
+for config_tag, maxiter in tqdm(configs.items()):
+    repetitions = 1
 
-cum_repros = []
-cumreps = []
-for rep in tqdm(range(repetitions)):
-    cum_runstats_dicts = []
-    total_elapsed = 0
-    cumdf =  pd.DataFrame()
-    reprod = {}
-    for zsh in tqdm(solv_hashes):
-        cmdstr = "python3 ./smtvm.py " + zsh[0] + " install " + zsh[1]
-        o = sp.getoutput(cmdstr)
+    base_cvc4 = "0675545"
+    base_z3 = "a35d00e"
 
-        if zsh[0].strip() == "z3":
-            cmdstr = "python3 ./smtvm.py cvc4 install " + base_cvc4
+    o = sp.getoutput("ls ~/known/repro").split("\n")
+    solv_hashes = [l.split("-") for l in o]
+
+    cum_repros = []
+    cumreps = []
+    for rep in tqdm(range(repetitions)):
+        cum_runstats_dicts = []
+        total_elapsed = 0
+        cumdf =  pd.DataFrame()
+        reprod = {}
+        for zsh in tqdm(solv_hashes):
+            cmdstr = "python3 ./smtvm.py " + zsh[0] + " install " + zsh[1]
             o = sp.getoutput(cmdstr)
-        elif zsh[0].strip() == "cvc4":
-            cmdstr = "python3 ./smtvm.py z3 install " + base_z3
-            o = sp.getoutput(cmdstr)
-        else:
-            sys.exit("didn't recognize solver version")
+
+            if zsh[0].strip() == "z3":
+                cmdstr = "python3 ./smtvm.py cvc4 install " + base_cvc4
+                o = sp.getoutput(cmdstr)
+            elif zsh[0].strip() == "cvc4":
+                cmdstr = "python3 ./smtvm.py z3 install " + base_z3
+                o = sp.getoutput(cmdstr)
+            else:
+                sys.exit("didn't recognize solver version")
 
 
-        cmdstr = "cswap-cli -v -s " + str(rep) + " -i 5 ~/known/repro/" + zsh[0] + "-" + zsh[1]
-        (s, o) = sp.getstatusoutput(cmdstr)
-        elapsed = int([l for l in o.split("\n") if "Elapsed Time" in l][0].split(':')[-1].strip())
-        total_elapsed = elapsed + total_elapsed
+            cmdstr = "cswap-cli -v -s " + str(rep) + " -i " + str(maxiter) + " ~/known/repro/" + zsh[0] + "-" + zsh[1]
+            (s, o) = sp.getstatusoutput(cmdstr)
+            elapsed = int([l for l in o.split("\n") if "Elapsed Time" in l][0].split(':')[-1].strip())
+            total_elapsed = elapsed + total_elapsed
 
-        runstats_str = [l for l in o.split("\n") if "JSONRUNSTATS:" in l][0].split("JSONRUNSTATS:")[-1]
-        runstats = json.loads(runstats_str)
-        cum_runstats_dicts.append(runstats)
+            runstats_str = [l for l in o.split("\n") if "JSONRUNSTATS:" in l][0].split("JSONRUNSTATS:")[-1]
+            runstats = json.loads(runstats_str)
+            cum_runstats_dicts.append(runstats)
 
-        cfg_str = [l for l in o.split("\n") if "JSONCONFIG:" in l][0].split("JSONCONFIG:")[-1]
-        cfg = json.loads(cfg_str)
-        del cfg["file_provider"]
-        cfg["profiles"] = len(cfg["profiles"])
-        dfcfg = pd.DataFrame.from_dict(cfg, orient="index")
+            cfg_str = [l for l in o.split("\n") if "JSONCONFIG:" in l][0].split("JSONCONFIG:")[-1]
+            cfg = json.loads(cfg_str)
+            del cfg["file_provider"]
+            cfg["profiles"] = len(cfg["profiles"])
+            dfcfg = pd.DataFrame.from_dict(cfg, orient="index")
 
-        cmdstr = "ls 0-cswap-fuzz-run-out/bugs"
-        bug_files = sp.getoutput(cmdstr).split("\n")
+            cmdstr = "ls 0-cswap-fuzz-run-out/bugs"
+            bug_files = sp.getoutput(cmdstr).split("\n")
 
-        cmdstr = "ls ~/known/repro/" + zsh[0] + "-" + zsh[1]
-        seed_files = sp.getoutput(cmdstr).split("\n")
-        seed_file_stems = [os.path.splitext(f)[0] for f in seed_files]
+            cmdstr = "ls ~/known/repro/" + zsh[0] + "-" + zsh[1]
+            seed_files = sp.getoutput(cmdstr).split("\n")
+            seed_file_stems = [os.path.splitext(f)[0] for f in seed_files]
 
-        for sfs in seed_file_stems:
-            solver_sfs = str(zsh[0]) + "-" + sfs
-            if solver_sfs not in reprod.keys():
-                reprod[solver_sfs] = "NOREPRO"
+            for sfs in seed_file_stems:
+                solver_sfs = str(zsh[0]) + "-" + sfs
+                if solver_sfs not in reprod.keys():
+                    reprod[solver_sfs] = "NOREPRO"
 
-            for bf in bug_files:
-                cmdstr = "cat 0-cswap-fuzz-run-out/bugs/" + bf
-                bf_cts = sp.getoutput(cmdstr).lower()
+                for bf in bug_files:
+                    cmdstr = "cat 0-cswap-fuzz-run-out/bugs/" + bf
+                    bf_cts = sp.getoutput(cmdstr).lower()
 
-                if sfs in bf:
-                    if "soundness" in bf_cts:
-                        reprod[solver_sfs] = "SOUND" 
-                    elif reprod[solver_sfs] != "SOUND":
-                        reprod[solver_sfs] = "BUG" 
-        cmdstr = "rm -r " + str(rep) + "-cswap-fuzz-run-out"
-        o = sp.getstatusoutput(cmdstr)
-
-
+                    if sfs in bf:
+                        if "soundness" in bf_cts:
+                            reprod[solver_sfs] = "SOUND" 
+                        elif reprod[solver_sfs] != "SOUND":
+                            reprod[solver_sfs] = "BUG" 
+            cmdstr = "rm -r " + str(rep) + "-cswap-fuzz-run-out"
+            o = sp.getstatusoutput(cmdstr)
 
 
 
-    reprocats = CategoricalDtype(categories=["BUG", "SOUND", "NOREPRO"])
-    reprodf = pd.DataFrame.from_dict(reprod, orient="index", dtype=reprocats)
-    cum_repros.append(reprodf[0])
 
-    cumdf = pd.DataFrame(cum_runstats_dicts)
 
-    iter_df = cumdf.sum()
-    iter_df["Bugs"] = reprodf[0].value_counts()["BUG"] + reprodf[0].value_counts()["SOUND"]
-    iter_df["Soundness Bugs"] = reprodf[0].value_counts()["SOUND"]
+        reprocats = CategoricalDtype(categories=["BUG", "SOUND", "NOREPRO"])
+        reprodf = pd.DataFrame.from_dict(reprod, orient="index", dtype=reprocats)
+        cum_repros.append(reprodf[0])
 
-    cumreps.append(iter_df)
+        cumdf = pd.DataFrame(cum_runstats_dicts)
 
-cumreps_repros_df = pd.DataFrame(cum_repros).reset_index(drop=True)
-cumrepsdf = pd.DataFrame(cumreps)
+        iter_df = cumdf.sum()
+        iter_df["Bugs"] = reprodf[0].value_counts()["BUG"] + reprodf[0].value_counts()["SOUND"]
+        iter_df["Soundness Bugs"] = reprodf[0].value_counts()["SOUND"]
+        iter_df["Elapsed Time"] = total_elapsed
 
-cumrepsdf.to_csv(config_tag + ".csv")
-res = pd.DataFrame()
-res["Means"] = cumrepsdf.mean()
-res["Std"] = cumrepsdf.std()
-print(res)
-# print(cumrepsdf.std())
+        cumreps.append(iter_df)
+
+    cumreps_repros_df = pd.DataFrame(cum_repros).reset_index(drop=True)
+    cumrepsdf = pd.DataFrame(cumreps)
+
+    cumrepsdf.to_csv(config_tag + ".csv")
+    res = pd.DataFrame()
+    res["Means"] = cumrepsdf.mean()
+    res["Std"] = cumrepsdf.std()
+    print(res)
+    # print(cumrepsdf.std())
 
